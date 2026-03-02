967dbad65d2df264a6d915d97e9b54bafcac5314
Prajith <prajithsiva07@gmail.com>
2026-03-02 19:53:39 +0530

    login system added

--- AI-cyber-guardian/frontend/src/App.jsx
+++ AI-cyber-guardian/frontend/src/App.jsx
@@ -1,11 +1,34 @@
+import { SignedIn, SignedOut, SignIn, UserButton } from "@clerk/clerk-react";
 import Dashboard from "./pages/Dashboard";
 import "./App.css";
 
 function App() {
   return (
-    <div>
-      <Dashboard />
-    </div>
+    <>
+      <SignedOut>
+        <div className="auth-container">
+          <div className="auth-left">
+            <h1>🛡 AI Cyber Guardian</h1>
+            <p>
+              Enterprise-grade AI security for detecting phishing,
+              malicious links, and corporate email threats in real time.
+            </p>
+          </div>
+
+          <div className="auth-right">
+            <SignIn />
+          </div>
+        </div>
+      </SignedOut>
+
+      <SignedIn>
+        <div className="topbar">
+          <h2>🛡 AI Cyber Guardian</h2>
+          <UserButton afterSignOutUrl="/" />
+        </div>
+        <Dashboard />
+      </SignedIn>
+    </>
   );
 }
 
